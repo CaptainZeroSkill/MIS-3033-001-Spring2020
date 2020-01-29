459c857430076cb60211bb55e361b1200ee55791
Braden Henson <braden.henson@ou.edu>
2020-01-29 10:45:38 -0600

    created the entry form class

---
+++ WPF1/WPF1/EntryForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF1
+{
+    class EntryForm
+    {
+        public EntryForm()
+        {
+           
+        }
+
+        public EntryForm(string name, string address, int zipCode)
+        {
+            name = Name;
+            address = Address;
+            zipCode = ZipCode;
+        }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public int ZipCode { get; set; }
+    }
+}
