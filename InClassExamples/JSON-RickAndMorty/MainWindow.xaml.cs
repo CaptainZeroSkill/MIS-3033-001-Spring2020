852d88dd317b2f00fa4efb45fd1132c5c59f4a6f
Braden Henson <braden.henson@ou.edu>
2020-02-10 11:42:59 -0600

    created a WPF application that brings in information to a list box about characters from Rick and Morty. Clicking on a character will display an image of that character

---
+++ InClassExamples/JSON-RickAndMorty/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_RickAndMorty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //@ means treat all characters as literal
+
+            string dataSetUrl = "https://rickandmortyapi.com/api/character/";
+
+
+            //HTTP Client code will be given on the exams, but need to know how to activate the proper using fuction to use properly
+
+
+            RickAndMortyAPI rickAndMortyStuff;
+            using (var client = new HttpClient())
+            {
+                //client.BaseAddress = new Uri(dataSetUrl);
+
+                //.Result is waiting for the result to happen
+                string json = client.GetStringAsync(dataSetUrl).Result;
+
+                //Add using Newtonsoft.Json in order to access the JsonConvert function
+                rickAndMortyStuff = JsonConvert.DeserializeObject<RickAndMortyAPI>(json);
+
+
+
+            }
+
+            //Need a loop to iterate through all of the results collection
+            foreach (var character in rickAndMortyStuff.results)
+            {
+                lstbxCharacters.Items.Add(character);
+            }
+        }
+
+        private void lstbxCharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Cast to treat as a ResultObject object
+            var selectedCharacterFromList = (ResultObject)lstbxCharacters.SelectedItem;
+
+            //JSON allows us to access someones' API without needing access to their database
+            imgCharacter.Source = new BitmapImage(new Uri(selectedCharacterFromList.image));
+        }
+    }
+}
---
+++ InClassExamples/JSON-RickAndMorty/RickAndMortyAPI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_RickAndMorty
+{
+    class RickAndMortyAPI
+    {
+        //Set getters and setters for the properties present in the JSON file
+        public InfoObject info { get; set; }
+
+        public List<ResultObject> results { get; set; }
+
+    }
+
+    public class ResultObject
+    {
+        //Not necessary to bring in every attribute of a given object, omitted properties will be skipped
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string status { get; set; }
+
+        public string species { get; set; }
+
+        public string gender { get; set; }
+
+        public string image { get; set; }
+
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return $"{id} {name}";
+        }
+    }
+
+    public class InfoObject
+    {
+        public int count { get; set; }
+
+        public int pages { get; set; }
+
+        public string next { get; set; }
+
+        public string prev { get; set; }
+    }
+}
