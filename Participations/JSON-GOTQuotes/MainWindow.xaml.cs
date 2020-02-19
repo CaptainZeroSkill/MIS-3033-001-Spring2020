58ff2c1a6709571d6205ce7420dccf8e850fe939
Braden Henson <braden.henson@ou.edu>
2020-02-19 11:22:44 -0600

    created a program that displays a random quote by a got chracter from a random quote generator api website through the use of JSON

---
+++ Participations/JSON-GOTQuotes/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_GOTQuotes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetQuote_Click(object sender, RoutedEventArgs e)
+        {
+            string dataSetURL = "https://got-quotes.herokuapp.com/quotes";
+
+            Quote gotQuote;
+
+            //httpclient allows use of things on the internet
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(dataSetURL).Result;
+
+                gotQuote = JsonConvert.DeserializeObject<Quote>(json);
+            }
+            MessageBox.Show($"{gotQuote.ToString()}");
+        }
+    }
+}
---
+++ Participations/JSON-GOTQuotes/Quote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_GOTQuotes
+{
+    class Quote
+    {
+        public Quote()
+        {
+
+        }
+
+        public string quote { get; set; }
+
+        public string character { get; set; }
+
+        public override string ToString()
+        {
+            return $"{quote}\n Character : {character}";
+        }
+    }
+}
